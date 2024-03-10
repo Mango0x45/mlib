776224c34ed894e63e3bcf7806c58090d2b72f4f
Thomas Voss <mail@thomasvoss.com>
2024-03-10 17:25:29 +0200

    Fix bugs in u8haspfx() and u8hassfx()

--- lib/mbstring/u8haspfx.c
+++ lib/mbstring/u8haspfx.c
@@ -1,9 +1,10 @@
 #include <string.h>
 
+#include "macros.h"
 #include "mbstring.h"
 
 bool
 u8haspfx(const char8_t *s, size_t n, const char8_t *pfx, size_t m)
 {
-	return n >= m && memcmp(s, pfx, m);
+	return n >= m && memeq(s, pfx, m);
 }
--- lib/mbstring/u8hassfx.c
+++ lib/mbstring/u8hassfx.c
@@ -1,9 +1,10 @@
 #include <string.h>
 
+#include "macros.h"
 #include "mbstring.h"
 
 bool
 u8hassfx(const char8_t *s, size_t n, const char8_t *sfx, size_t m)
 {
-	return n >= m && memcmp(s + n - m, sfx, m);
+	return n >= m && memeq(s + n - m, sfx, m);
 }
