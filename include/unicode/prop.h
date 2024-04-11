fcebbdb6a88c544a4c1e41039ce1ffb662e22cfa
Thomas Voss <mail@thomasvoss.com>
2024-04-11 18:27:41 +0200

    Add uprop_get_na1()

--- include/unicode/prop.h
+++ include/unicode/prop.h
@@ -152,6 +152,7 @@ enum uprop_nt {
 [[__nodiscard__, __unsequenced__]] enum uprop_nt uprop_get_nt(rune);
 [[__nodiscard__, __unsequenced__]] rune uprop_get_bpb(rune);
 [[__nodiscard__, __unsequenced__]] struct u8view uprop_get_na(rune);
+[[__nodiscard__, __unsequenced__]] struct u8view uprop_get_na1(rune);
 
 /* PROP PREDICATES START */
 [[__nodiscard__, __unsequenced__]] bool uprop_is_ahex(rune);
