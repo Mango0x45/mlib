ca35292bf9eb91f75630c577990a581cba4a30ad
Thomas Voss <mail@thomasvoss.com>
2024-04-27 22:20:07 +0200

    Lock and unlock stderr while printing diagnostics

--- lib/errors/vwarn.c
+++ lib/errors/vwarn.c
@@ -9,9 +9,11 @@ void
 vwarn(const char *fmt, va_list ap)
 {
 	int save = errno;
+	flockfile(stderr);
 	fprintf(stderr, "%s: ", mlib_progname());
 	vfprintf(stderr, fmt, ap);
 	if (fmt[strlen(fmt) - 1] == ':')
 		fprintf(stderr, " %s", strerror(save));
 	fputc('\n', stderr);
+	funlockfile(stderr);
 }
