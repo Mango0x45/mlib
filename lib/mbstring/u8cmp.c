c0d2a0be9c95cc38818810207986818b333bc1de
Thomas Voss <mail@thomasvoss.com>
2024-05-10 23:26:51 +0200

    Fix bug detected by GCC sanitizer

--- lib/mbstring/u8cmp.c
+++ lib/mbstring/u8cmp.c
@@ -2,8 +2,13 @@
 
 #include "mbstring.h"
 
+/* Technically you can’t pass nullptr to memcmp(), so we check for x.len and
+   y.len both being 0 */
+
 int
 u8cmp(struct u8view x, struct u8view y)
 {
-	return x.len != y.len ? (x.len > y.len ? +1 : -1) : memcmp(x.p, y.p, x.len);
+	if (x.len != y.len)
+		return x.len > y.len ? +1 : -1;
+	return x.len == 0 && y.len == 0 ? 0 : memcmp(x.p, y.p, x.len);
 }
